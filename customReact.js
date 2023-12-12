127af920ac6b1acc8a97efd6037c38672582a1e0
Soumyendu <cyberrrnaut@gmail.com>
2023-12-12 16:03:20 +0530

    final commit

--- customReact.js
+++ customReact.js
@@ -1,35 +1,56 @@
-// Main.jsx
+// // Main.jsx
+
+// const reactElement = {
+//     type : 'a',
+//     props : {
+//         href: "https://github.com/cyberrrnaut/customReact"
+//     },
+//     children : "click me to visit Repo"
+// };
+
+// const customRender = function(reactElement,mainContainer){
+
+//            // document.createElement('p')
+//           const domElement = document.createElement(reactElement.type)
+
+//            domElement.innerHTML = reactElement.children;
+//            domElement.setAttribute('href',reactElement.props.href)
+
+//            mainContainer.appendChild(domElement);    //*main point* injecting the domelement or elements created by us using innerHTML and setAttribute
+
+//  };
+
+// const mainContainer = document.querySelector("#root");
+
+// customRender(reactElement,mainContainer);
+
+// const App= (reactElement)=>{
+//     reactElement = "div"
+// }
+
+const MainConatiner = document.querySelector("#root");
 
 const reactElement = {
-    type : 'a',
-    props : {
-        href: "https://github.com/cyberrrnaut/customReact"
-
-    },
-    children : "click me to visit Repo"
+  type: "a",
+  props: {
+    href: "https://github.com/cyberrrnaut/customReact",
+  },
+  children: "Click here to visit the repo",
 };
+const customRender = function(MainConatiner, reactElement){
+//   const DOM = document.createElement(reactElement.type);
+//   DOM.innerHTML = reactElement.children;
+//   DOM.setAttribute('href',reactElement.props.href);
+   
+   const dom =  document.createElement(reactElement.type);
+    dom.innerHTML = reactElement.children;
+    for (const prop in reactElement.props){
+        if(prop === 'children') continue;
+        dom.setAttribute(prop,reactElement.props[prop])
+    }
 
-const customRender =function(reactElement,mainContainer){
+  MainConatiner.appendChild(dom);
+};      
     
-           // document.createElement('p')
-          const domElement = document.createElement(reactElement.type)
-           
+customRender(MainConatiner, reactElement);
 
-           domElement.innerHTML = reactElement.children;
-           domElement.setAttribute('href',reactElement.props.href)
-
-           mainContainer.appendChild(domElement);
-           
- };
-
-const mainContainer = document.querySelector("#root");
-
-
-customRender(reactElement,mainContainer);
-
-
-
-
-
-
-const App= ()=>{}
